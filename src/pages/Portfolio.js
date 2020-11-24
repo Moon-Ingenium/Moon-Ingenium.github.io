e3f00d80eb7bed062bc4c63edf17512f7b9869e4
Michelle Moon <robbins.michelle@ymail.com>
2020-11-23 21:49:17 -0600

    fixed typo

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -19,36 +19,36 @@ function Portfolio(props) {
                         title="The Hunger Game"
                         href="https://s2hon.github.io/TheHungerGame/"
                         src="./assests/TheHungerGame.png"
-                        alt="Project1" className="img-thumbnail" />
+                        alt="Project1" />
                     <Project
                         title="Keep Austin Social"
                         href="https://enigmatic-brushlands-64346.herokuapp.com/"
                         src="./assests/KeepAustinSocial.png"
-                        alt="Project2" className="img-thumbnail" />
+                        alt="Project2" />
                 </Row>
                 <Row >
                     <Project
                         title="React Example"
                         href="https://moon-ingenium.github.io/Employee-Directory/"
                         src="./assests/EmployeeDirectory.png"
-                        alt="REACTapp" className="img-thumbnail" />
+                        alt="REACTapp" />
                     <Project
                         title="Weather Dashboard"
                         href="https://moon-ingenium.github.io/Weather-Dashboard/"
                         src="./assests/Weather-Dashboard.png"
-                        alt="weather forecast" className="img-thumbnail" />
+                        alt="weather forecast" />
                 </Row>
                 <Row >
                     <Project
                         title="Password Generator"
                         href="https://moon-ingenium.github.io/Password-Generator/"
                         src="./assests/password-generator.png"
-                        alt="A button generates a random string of characters" className="img-thumbnail" />
+                        alt="A button generates a random string of characters" />
                     <Project
                         title="Note Taker"
                         href="https://evening-refuge-24096.herokuapp.com/notes"
                         src="./assests/note-taker.png"
-                        alt="A note pad" className="img-thumbnail" />
+                        alt="A note pad" />
                 </Row >
             </Container>
             <Footer />
