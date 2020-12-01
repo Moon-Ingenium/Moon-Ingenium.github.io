48fe76fa17759b57f2accc98caa0a092595eca02
Michelle Moon <robbins.michelle@ymail.com>
2020-11-30 18:56:19 -0600

    icons for contact page

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,13 +8,11 @@ function Card(props) {
                 Contact
         </div>
             <div className="card-body">
-                <ul>
-                    <li className="list-group-item">robbins.michelle@ymail.com</li>
-                    <li className="list-group-item">817-564-1592</li>
-                    <li className="list-group-item"><a href="https://drive.google.com/file/d/1fuvtVmJMDgSXBI6lATkYc6IPvqby-qZL/view?usp=sharing">Resume</a></li>
-                    <li className="list-group-item"><a href="https://www.linkedin.com/in/michelle-moon-4123541ab/"> LinkedIn</a></li>
-                    <li className="list-group-item"><a href="https://github.com/Moon-Ingenium">GitHub</a></li>
-                </ul>
+                <p><a label="Email" value="Email" href="mailto:robbins.michelle@ymail.com" rel="noopener noreferer" className="fa fa-envelope"> </a></p>
+                    <p>817-564-1592</p>
+                    <p><a href="https://drive.google.com/file/d/1fuvtVmJMDgSXBI6lATkYc6IPvqby-qZL/view?usp=sharing"><i class="fa fa-file"></i></a></p>
+                    <p><a href="https://www.linkedin.com/in/michelle-moon-4123541ab/"><i class="fa fa-linkedin"></i></a></p>
+                    <p><a href="https://github.com/Moon-Ingenium"><i class="fa fa-github-square"></i></a></p>
             </div>
         </div>
     );
