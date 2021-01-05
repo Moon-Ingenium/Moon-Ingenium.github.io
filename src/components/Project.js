8d595b29a77846e02104dc1e28d642db07ad54a6
Michelle Moon <robbins.michelle@ymail.com>
2021-01-05 10:10:41 -0600

    adding project summary

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,12 +6,15 @@ function Project(props) {
 
     return (
         <Col size={"md"} number={6}>
-            <a href={props.link} style={{color:"black"}} target={"_blank"} rel="noreferrer">
-                <h3>{props.title}</h3>
+            <a href={props.link} style={{ color: "black" }} target={"_blank"} rel="noreferrer">
+                <h3>{props.title}<i className="fa fa-github project-link" style={{ padding: "20px" }}></i>
+                </h3>
             </a>
             <a href={props.href} target={"_blank"} rel="noreferrer">
-                <img src={props.src} alt={props.alt} className="img-thumbnail" />
+                <img src={props.src} alt={props.alt} className="img-thumbnail" style={{ marginBottom: "20px" }} />
             </a>
+            <p className="summary">{props.summary}</p>
+
         </Col>
     )
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -18,14 +18,17 @@ function Portfolio(props) {
                         link="https://github.com/Moon-Ingenium/sushi-junai"
                         href="https://sushi-junai.herokuapp.com/"
                         src="./assests/sushi-junai.png"
-                        alt="sushi restraunt menu" />
+                        alt="sushi restraunt menu" 
+                        summary="Sushi Junai's web app is a mobile first interactive menu for the restaurant's all-you-can-eat customers. Diners can add items to their order summary, see the amount those items would've cost, and calculate tip. A timer shows diners how long they have left in their two hour all-you-can-eat dining time. Diners also have the option to create an account and save menu items to their favorites for easy access the next time they dine at Sushi Junai."/>
                     <Project
                         title="Keep Austin Social"
                         link="https://github.com/Moon-Ingenium/KeepAustinSocial"
                         href="https://enigmatic-brushlands-64346.herokuapp.com/"
                         src="./assests/KeepAustinSocial.png"
-                        alt="Project2" />
-
+                        alt="Project2" 
+                        summary="A Full Stack app for locals to find reliable information on happy hours, post and interact with other users, and search for businesses by name or zipcode.​"
+                        />
+                        
                 </Row>
                 <Row >
                     <Project
@@ -33,13 +36,17 @@ function Portfolio(props) {
                         link="https://github.com/Moon-Ingenium/Employee-Directory"
                         href="https://moon-ingenium.github.io/Employee-Directory/"
                         src="./assests/EmployeeDirectory.png"
-                        alt="REACTapp" />
+                        alt="REACTapp"
+                        summary="An Employee directory app used to sort employee positions and organize employee information such as ID, name, occupation and email"
+                         />
                     <Project
                         title="Weather Dashboard"
                         link="https://github.com/Moon-Ingenium/Weather-Dashboard"
                         href="https://moon-ingenium.github.io/Weather-Dashboard/"
                         src="./assests/Weather-Dashboard.png"
-                        alt="weather forecast" />
+                        alt="weather forecast" 
+                        summary="A weather dashboard for a user to search multiple cities and plan a trip accordingly. Each city diplays current and future conditions. "
+                        />
                 </Row>
                 <Row >
                     <Project
@@ -47,13 +54,17 @@ function Portfolio(props) {
                         link="https://github.com/Moon-Ingenium/TheHungerGame"
                         href="https://s2hon.github.io/TheHungerGame/"
                         src="./assests/TheHungerGame.png"
-                        alt="Project1" />
+                        alt="Project1" 
+                        summary="A Front End app intended to make the meal plan process easier. A user can searh health criteria and return random meal recipes, save ingriedents, and search for the nearest grocery store."
+                        />
                     <Project
                         title="Password Generator"
                         link="https://github.com/Moon-Ingenium/Password-Generator"
                         href="https://moon-ingenium.github.io/Password-Generator/"
                         src="./assests/password-generator.png"
-                        alt="A button generates a random string of characters" />
+                        alt="A button generates a random string of characters" 
+                        summary="An app to create a random password based off the user's strength criterias. "
+                        />
                 </Row>
                 <Row >
                     <Project
@@ -61,7 +72,8 @@ function Portfolio(props) {
                         link="https://github.com/Moon-Ingenium/Note-Taker"
                         href="https://evening-refuge-24096.herokuapp.com/notes"
                         src="./assests/note-taker.png"
-                        alt="A note pad" />
+                        alt="A note pad"
+                        summary="A full stack application that can be used to write, save, and delete notes" />
                 </Row >
             </Container>
             <Footer />
